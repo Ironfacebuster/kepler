28f23d6e99b501724383c414b37d3a87ce51e333
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2021-07-10 20:40:07 -0400

    Misc cleanup

--- helpers/StateMachine.cs
+++ helpers/StateMachine.cs
@@ -683,7 +683,7 @@ namespace StateMachine
             booleans["declared_variable"] = false;
             booleans["declared_function"] = false;
             booleans["variable_assign"] = false;
-            booleans["inside_string"] = false;
+            // booleans["inside_string"] = false;
             booleans["function_assign"] = false;
             booleans["assigning_function_variables"] = false;
             booleans["assigning_function_variables_type"] = false;
@@ -699,7 +699,7 @@ namespace StateMachine
 
         void AssignDefaultStrings()
         {
-            strings["build_string"] = "";
+            // strings["build_string"] = "";
             strings["nonpositional_variable_name"] = "";
             strings["print_string"] = "";
         }
