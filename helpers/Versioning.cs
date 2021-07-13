aba2f77173959254239eababf0651de63585d6d3
Ironfacebuster <willeston202@gmail.com>
2021-07-13 14:13:28 -0400

    Update macro replacement

--- helpers/Versioning.cs
+++ helpers/Versioning.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Text.RegularExpressions;
+using KeplerTokenizer;
+using KeplerTokens.Tokens;
 
 namespace KeplerVersioning
 {
@@ -7,11 +10,27 @@ namespace KeplerVersioning
         public static string _VERSION = "v1a1.1";
         public static string _RELEASE = "July 13th, 2021";
 
-        public static string ReplaceMacros(string line)
+        public static LineIterator ReplaceMacros(LineIterator line)
         {
             // use the static values above
             // and replace macros like $_VERSION
-            line = new Regex(@"(?<!^)(\$_VERSION)").Replace(line, EscapeString(_VERSION));
+
+            // TODO: make this more elegant
+            for (var i = 0; i < line.tokens.Count; ++i)
+            {
+                Token t = line.tokens[i];
+
+                if (t.start != 0)
+                {
+                    switch (t.token_string)
+                    {
+                        case "$_VERSION":
+                            line.tokens[i] = new Token(TokenType.StaticString, t.start, EscapeString(_VERSION));
+                            break;
+                    }
+                }
+            }
+
             return line;
         }
 
