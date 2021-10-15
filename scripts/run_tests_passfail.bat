07936f09c10f814c3db22d38b97b541c287d98c9
Ironfacebuster <willeston202@gmail.com>
2021-10-15 14:05:06 -0400

    Fix action

--- scripts/run_tests_passfail.bat
+++ scripts/run_tests_passfail.bat
@@ -9,6 +9,10 @@ echo.
 for %%f in ("%~dp0..\tests\*.kep") do (
 
     setlocal
+    for %%i in ("%%f") do (
+        rem echo file without newline
+        <nul set /p ="%%~ni..."
+    )
 
     set "RESULT="
     
@@ -20,11 +24,31 @@ for %%f in ("%~dp0..\tests\*.kep") do (
     rem if the command doesn't print "SUCCESS" it failed!
     rem this also handles other errors, such as exiting without printing, etc.
     if "!RESULT!"=="SUCCESS" (
+        echo PASSED
         set /A PASSED=PASSED+1
     ) else (
+        echo FAILED
         set /A FAILED=FAILED+1
     )
 )
+
+echo.
+echo =RESULTS================
+echo.
+
+set /A TOTAL=PASSED+FAILED
+
+echo Total Tests: !TOTAL!
+echo.
+
+echo Pass/Fail: !PASSED!/!FAILED!
+
+set /A f_passed=PASSED*100
+set /A PERCENT=f_passed/TOTAL
+
+echo Percent Completion: !PERCENT!%% 
+echo.
+echo ========================
 
 rem if failed is greater than zero, exit with an error 
 if !FAILED! gtr 0 (
