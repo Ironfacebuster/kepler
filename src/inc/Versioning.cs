1cecdc59771df49e1b8c69134c8de32f7d1ed077
Ironfacebuster <willeston202@gmail.com>
2021-10-22 06:38:03 -0400

    Increment version string

--- src/inc/Versioning.cs
+++ src/inc/Versioning.cs
@@ -7,7 +7,7 @@ namespace Kepler.Versioning
 {
     public class StaticValues
     {
-        public static string _VERSION = "v1a1.3.3";
+        public static string _VERSION = "v1a1.4";
         public static string _TYPE = Resources.type;
         // public static string _RELEASE = "October 13th, 2021";
         public static string _RELEASE = Resources.buildtime;
