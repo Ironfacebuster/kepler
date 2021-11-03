a8f1dfb04a4bafcbf5168f5b1addaa1d0cc26f17
Ironfacebuster <willeston202@gmail.com>
2021-11-03 19:47:40 -0400

    Flesh out filesystem module

--- src/lib/filesystem.cs
+++ src/lib/filesystem.cs
@@ -9,17 +9,31 @@ namespace Kepler.Modules
         {
 
             // FILESYSTEM MODULE
-            KeplerFunction fs_load = new KeplerFunction("fs_load", true);
-            fs_load.SetType(KeplerType.String);
-            fs_load.internal_call = (interpreter, args) =>
+            KeplerFunction fs_read = new KeplerFunction("fs_read", true);
+            fs_read.SetType(KeplerType.String);
+            fs_read.AssignNonPositional("file", KeplerType.String);
+            fs_read.internal_call = (interpreter, args) =>
             {
-                KeplerVariable res = new KeplerVariable();
-                res.SetStringValue("null");
-                res.SetModifier(KeplerModifier.Constant);
+                string filename = args.GetArgument("file").GetValueAsString();
 
-                // TODO: actually implement this
+                KeplerFunction create_obj = interpreter.statemachine.functions.GetFunction("object_create", true);
+                KeplerVariable object_id = create_obj.internal_call(interpreter, create_obj.arguments);
 
-                return res;
+                KeplerFunction set_obj = interpreter.statemachine.functions.GetFunction("object_set", true);
+
+                // Set arguments
+                set_obj.SetNonPositional("id", object_id.Clone()); // for some reason I have to clone this variable to avoid a type error
+                set_obj.SetNonPositional("key", new KeplerVariable() { StringValue = "name", type = KeplerType.String });
+                set_obj.SetNonPositional("value", new KeplerVariable() { StringValue = filename, type = KeplerType.String });
+                set_obj.internal_call(interpreter, set_obj.arguments); // set name key
+
+                set_obj.SetNonPositional("key", new KeplerVariable() { StringValue = "data", type = KeplerType.String });
+                set_obj.SetNonPositional("value", new KeplerVariable() { StringValue = "", type = KeplerType.String });
+                set_obj.internal_call(interpreter, set_obj.arguments); // set data key
+
+                // TODO: actually finish this
+
+                return object_id;
             };
 
             KeplerFunction fs_exists = new KeplerFunction("fs_exists", true);
@@ -27,6 +41,7 @@ namespace Kepler.Modules
             fs_exists.AssignNonPositional("file", KeplerType.String);
             fs_exists.internal_call = (interpreter, args) =>
             {
+                string filename = args.GetArgument("file").GetValueAsString();
                 KeplerVariable res = new KeplerVariable();
                 res.SetBoolValue(false);
                 res.SetModifier(KeplerModifier.Constant);
@@ -38,8 +53,15 @@ namespace Kepler.Modules
 
             KeplerFunction fs_write = new KeplerFunction("fs_write", true);
             fs_write.SetType(KeplerType.Int);
+            fs_write.AssignNonPositional("file", KeplerType.String);
+            fs_write.AssignNonPositional("data", KeplerType.Any);
             fs_write.internal_call = (interpreter, args) =>
             {
+                string filename = args.GetArgument("file").GetValueAsString();
+
+                // convert all data to a string
+                string data = args.GetArgument("data").GetValueAsString();
+
                 KeplerVariable res = new KeplerVariable();
                 // codes 0 = success, 1 = general error
                 res.SetIntValue(0);
@@ -50,7 +72,7 @@ namespace Kepler.Modules
                 return res;
             };
 
-            module = new Module("filesystem", new KeplerFunction[] { fs_load, fs_write, fs_exists });
+            module = new Module("filesystem", new KeplerFunction[] { fs_read, fs_write, fs_exists });
             module.AddRequiredModule(KObjects.module);
         }
     }
